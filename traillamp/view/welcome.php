2b22c85e03dcc617984340c559c7ca816f5a31fa
Apaxaa Studios <91748627+phaxadstudios@users.noreply.github.com>
2021-12-01 20:52:46 +0100

    Update welcome.php

--- traillamp/view/welcome.php
+++ traillamp/view/welcome.php
@@ -5,11 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../view/css/style.css"/>
-    <title>TrailLamp |Welcome</title>
+    <title>TrailLamp | Welcome</title>
    
 </head>
 <body>
-    <center class="welcome"><h2>Welcome To TrailLamp</h2><p>V.1.0.0</center>
-   
+    <center class="welcome">
+        //this image can be used...
+        //<img src="https://cdn-icons.flaticon.com/png/128/3172/premium/3172795.png?token=exp=1638389188~hmac=60ab520e0913384cb2271fc2190f558b"/>
+        <h2>Welcome To TrailLamp</h2>
+        <p>V.1.0.0</p>
+    </center>  
 </body>
 </html>
